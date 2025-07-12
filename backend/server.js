ebac552cf6492733a690236d072ba27ae655542e
kalyan021004 <kalyan021004@gmail.com>
2025-07-12 20:39:37 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "https://todo-board-five.vercel.app" || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
