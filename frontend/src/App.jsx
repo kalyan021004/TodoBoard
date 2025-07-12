4158198111133377d30f0f56ab34ddcce7bb2eaa
kalyan021004 <kalyan021004@gmail.com>
2025-07-12 20:34:12 +0530

    ch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { SocketProvider } from './context/SocketContext';
 import PrivateRoute from './components/PrivateRoute';
@@ -15,6 +15,9 @@ function App() {
         <Router>
           <div className="App">
             <Routes>
+              {/* Redirect from / to /login */}
+              <Route path="/" element={<Navigate to="/login" />} />
+
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route 
